00037280e2f5b17c7c17aa21a893ff4b8c0637a5
gordo1998 <jordibarreda98@gmail.com>
2024-06-16 23:34:36 +0200

    hola

---
+++ BDFreeze.sql
@@ -0,0 +1,46 @@
+create database Freeze;
+use Freeze;
+
+create table users
+(
+id int auto_increment,
+email varchar(100) not null,
+passwd varchar(300) not null,
+constraint pk_users primary key (id)
+);
+
+create table inventario
+(
+id int auto_increment,
+codigo_barra varchar(100),
+nombre varchar(300),
+imagen longblob,
+cantidad int,
+usuario int,
+constraint pk_inventario primary key (id),
+constraint fk_inventario_user foreign key (usuario) references users(id)
+);
+
+create table lista_compra
+(
+id int auto_increment,
+codigo_barra varchar(100),
+nombre varchar(300),
+imagen longblob,
+cantidad int,
+usuario int,
+constraint pk_inventario primary key (id),
+constraint fk_lista_compra_user foreign key (usuario) references users(id)
+);
+
+create table productos_comprados
+(
+id int auto_increment,
+codigo_barra varchar(100),
+nombre varchar(300),
+imagen longblob,
+cantidad int,
+usuario int,
+constraint pk_inventario primary key (id),
+constraint fk_productos_comprados_user foreign key (usuario) references users(id)
+);
