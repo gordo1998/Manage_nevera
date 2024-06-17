dcc74b1188ad2d12cbc7b219157809c5de7badc2
gordo1998 <jordibarreda98@gmail.com>
2024-06-17 12:14:52 +0200

    aa

--- BDFreeze.sql
+++ BDFreeze.sql
@@ -29,7 +29,7 @@ nombre varchar(300),
 imagen longblob,
 cantidad int,
 usuario int,
-constraint pk_inventario primary key (id),
+constraint pk_lista_compra primary key (id),
 constraint fk_lista_compra_user foreign key (usuario) references users(id)
 );
 
@@ -41,6 +41,18 @@ nombre varchar(300),
 imagen longblob,
 cantidad int,
 usuario int,
-constraint pk_inventario primary key (id),
+constraint pk_productos_comprados primary key (id),
 constraint fk_productos_comprados_user foreign key (usuario) references users(id)
+);
+
+create table productos_escaneados
+(
+id int auto_increment,
+codigo_barra varchar(100),
+nombre varchar(300),
+imagen longblob,
+cantidad int,
+usuario int,
+constraint pk_producto_escaneados primary key (id),
+constraint fk_productos_escaneados_user foreign key (usuario) references users(id)
 );
