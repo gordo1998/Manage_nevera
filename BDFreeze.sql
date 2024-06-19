e98ebfb62e158dcfa26526753448d7ecc3a86df8
gordo1998 <jordibarreda98@gmail.com>
2024-06-19 17:38:59 +0200

    base de datos modificada

--- BDFreeze.sql
+++ BDFreeze.sql
@@ -1,3 +1,4 @@
+drop database Freeze;
 create database Freeze;
 use Freeze;
 
@@ -9,50 +10,45 @@ passwd varchar(300) not null,
 constraint pk_users primary key (id)
 );
 
-create table inventario
-(
-id int auto_increment,
-codigo_barra varchar(100),
-nombre varchar(300),
-imagen longblob,
-cantidad int,
-usuario int,
-constraint pk_inventario primary key (id),
-constraint fk_inventario_user foreign key (usuario) references users(id)
-);
-
-create table lista_compra
-(
-id int auto_increment,
-codigo_barra varchar(100),
-nombre varchar(300),
-imagen longblob,
-cantidad int,
-usuario int,
-constraint pk_lista_compra primary key (id),
-constraint fk_lista_compra_user foreign key (usuario) references users(id)
-);
-
-create table productos_comprados
-(
-id int auto_increment,
-codigo_barra varchar(100),
-nombre varchar(300),
-imagen longblob,
-cantidad int,
-usuario int,
-constraint pk_productos_comprados primary key (id),
-constraint fk_productos_comprados_user foreign key (usuario) references users(id)
-);
-
 create table productos_escaneados
 (
 id int auto_increment,
 codigo_barra varchar(100),
 nombre varchar(300),
 imagen longblob,
+constraint pk_producto_escaneados primary key (id)
+);
+
+create table inventario
+(
+id int auto_increment,
 cantidad int,
 usuario int,
-constraint pk_producto_escaneados primary key (id),
-constraint fk_productos_escaneados_user foreign key (usuario) references users(id)
+producto int,
+constraint pk_inventario primary key (id),
+constraint fk_inventario_user foreign key (usuario) references users(id),
+constraint fk_inventario_product foreign key (producto) references productos_escaneados(id)
 );
+
+create table lista_compra
+(
+id int auto_increment,
+cantidad int,
+usuario int,
+producto int,
+constraint pk_lista_compra primary key (id),
+constraint fk_lista_compra_user foreign key (usuario) references users(id),
+constraint fk_lista_compra_product foreign key (producto) references productos_escaneados(id)
+);
+
+create table productos_comprados
+(
+id int auto_increment,
+cantidad int,
+usuario int,
+producto int,
+constraint pk_productos_comprados primary key (id),
+constraint fk_productos_comprados_user foreign key (usuario) references users(id),
+constraint fk_productos_comprados_product foreign key (producto) references productos_escaneados(id)
+);
+
